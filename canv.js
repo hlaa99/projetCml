26de91f2ecaa9f5044e6cc61e4d9d48ac4f9bdbb
Biggy123123 <kensley.bertrand@gmail.com>
2021-09-22 10:15:06 -0700

    La partie javascript

---
+++ canv.js
@@ -0,0 +1,156 @@
+  var can = document.getElementById("canvas");
+					   var context = can.getContext("2d");
+					    
+						
+						 //chemine
+					  context.beginPath();
+						context.moveTo(400,394);
+						context.lineTo(400,375);
+						context.lineTo(430,375);
+						context.lineTo(430,414);
+					  context.stroke();
+					  context.fillStyle="#ffd66b";
+					  context.fill();
+					  context.closePath();
+					  
+					  context.beginPath();
+						context.moveTo(385,375);
+						context.lineTo(445,375);
+						context.lineTo(445,360);
+						context.lineTo(385,360);
+						context.lineTo(385,375);
+					  context.stroke();
+					  context.fillStyle="#6359ff";
+					  context.fill();
+					  context.closePath();
+						
+						//trois 1 
+						context.beginPath();
+					  context.moveTo(160,526);
+					  context.lineTo(350,400);
+					  context.lineTo(540,526);
+					  context.lineTo(550,510);
+					  context.lineTo(350,380);
+					  context.lineTo(150,510);
+					  context.lineTo(160,526);
+					  context.stroke();
+					  context.fillStyle="#6359ff";
+					  context.fill();
+					  context.closePath();
+					  
+					  //trois 2 
+						context.beginPath();
+						context.moveTo(150,510);
+						context.lineTo(350,380);
+					    context.lineTo(550,510);
+						context.lineTo(559,495);
+						context.lineTo(350,360);
+						context.lineTo(140,495);
+						context.lineTo(150,510);
+					  context.stroke();
+					  context.fillStyle="#000099"
+					  context.fill();
+					  context.closePath();
+					  
+					 
+					  
+					  
+						//Maison
+						context.beginPath();
+					  context.moveTo(350,400);
+					  context.lineTo(500,500);
+					  context.lineTo(500,700);
+					  context.lineTo(200,700);
+					  context.lineTo(200,500);
+					  context.lineTo(350,400);
+					  context.stroke();
+					  context.fillStyle="#ffd66b";
+					  context.fill();
+					  context.closePath();
+					  context.strokeStyle="bleu";
+					  context.fill();
+						 
+					 
+					  
+					  //Trianle
+					  context.beginPath();
+					  context.moveTo(400,480);
+					  context.lineTo(350,430);
+					  context.lineTo(300,480);
+					  context.lineTo(400,480);
+					  context.stroke();
+					  context.fillStyle="#beefff";
+					  context.closePath();
+					  context.fill();
+					  
+					  
+					  
+					  //Ligne dans triangle ver
+					  context.beginPath();
+					  context.rect(350,430,0,51);
+					  context.stroke();
+					  context.closePath();
+					  context.fill();
+					  
+					  //Ligne dans triangle horiz
+					  context.beginPath();
+					  context.rect(335,445,31,0);
+					  context.stroke();
+					  context.closePath();
+					  context.fill();
+					  
+				
+					 					   
+					    
+					  
+				
+					  	 			 		  
+					 	//les carrees  
+					 context.beginPath();
+					  context.rect(320,560,70,100);
+					  context.stroke();
+					  context.fillStyle="##beefff";
+					  context.closePath();
+					  context.fill();
+					  
+					  context.beginPath();
+					  context.rect(430,560,50,100);
+					  context.stroke();
+					  context.fillStyle="##beefff";
+					  context.closePath();
+					  context.fill();
+					  
+					  
+					  //fenetre
+					  context.beginPath();
+					  context.rect(230,560,50,100);
+					  context.stroke();
+					  context.fillStyle="#beefff";
+					  context.closePath();
+					  context.fill();
+					  
+					  
+					  //droite porte
+					  context.beginPath();
+					  context.rect(355,560,0,100);
+					  context.stroke();
+					  context.closePath();
+					  context.fill();
+					  
+					  
+					  //carree porte
+					   context.beginPath();
+					  context.rect(320,650,69,10);
+					  context.stroke();
+					  context.fillStyle="orange";
+					  context.closePath();
+					  context.fill();
+					  
+					
+					  
+					  
+					  
+					 					   
+					  
+									
+					 	
